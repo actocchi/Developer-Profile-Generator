b73554a0bd804dd9bba68cf4d1aeb83815c9df78
Toki <actocchi@gmail.com>
2019-10-10 16:49:13 -0700

    psuedo code and pulls most info

--- index.js
+++ index.js
@@ -3,63 +3,67 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 
 const questions = [
-  
+
 ];
 
 // function writeToFile(fileName, data) {
- 
+
 // };
 
 function init() {
-// make inquirer function and axios function to have in here
-inquirer
-  .prompt([
-    {
-    message: "Enter your GitHub username",
-    name: "username"
-  },
-  {
-    message: "chose a color",
-    name: "color"
-  }
-  ])
-  .then(function (userResponses) {
-    console.log(userResponses); // {username: 'atoci', color: 'blue'}
-    let {username, color} = userResponses;
+  // make inquirer function and axios function to have in here
+  inquirer
+    .prompt([
+      {
+        message: "Enter your GitHub username",
+        name: "username"
+      },
+      {
+        message: "chose a color",
+        name: "color"
+      }
+    ])
+    .then(function (userResponses) {
+      console.log(userResponses); // {username: 'atoci', color: 'blue'}
+      let { username, color } = userResponses;
 
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-  
-   //  axios get call
-    axios
-      .get(queryUrl)
-      // wait for response
-      .then(function (res) {
-      
-        //  logs data from call
-        // runs a for each for each object/repo 
-        res.data.forEach(repo => {
-          questions.push(repo.name); //pushes specific name to names array that i made in global variables
+      const queryUrl = `https://api.github.com/users/${username}`;
+
+      //  axios get call
+      axios
+        .get(queryUrl)
+        // wait for response
+        .then(function (res) {
+
+          //  logs data from call
+          // runs a for each for each object/repo 
+
+          // link these to the other factors and not to repos
+          // res.data.forEach(repo => {
+           
+          questions.push(res.data.name, res.data.location, res.data.bio); //pushes specific name to names array that i made in global variables
+
+
+            // need response link to - res.data.location - res.data.html_url - res.data.blog /  res.data.avatar_url  res.data.name res.data.public_repos res.data.followers res.data.following 
+          // });
+          data = questions.join("\n"); //turn the array names into a string named nameJSON
+          // writes files to new JSONfile named nameJSON and console logs if you did it right
+          console.log(data);
+
+          fs.writeFile("names.txt", data, err => {
+            if (err) {
+              throw err;
+            }
+            // check to see if there were errors on writing
+            console.log("Successfully wrote to names.txt file");
+          });
 
         });
-                                          // what does this 2 dooo? it formats based off of tabs
-        repoNames = questions.join("\n"); //turn the array names into a string named nameJSON
-        // writes files to new JSONfile named nameJSON and console logs if you did it right
-       console.log(repoNames);
-        
-       fs.writeFile("names.txt", repoNames, err => {
-          if (err) {
-            throw err;
-          }
-          // check to see if there were errors on writing
-          console.log("Successfully wrote to names.txt file");
-        });
-
-      });
-  });
+    });
 
 };
 
-init(); 
+init();
 
 // call generate html after axios call has gotten a response
 // TODO: create body in html
